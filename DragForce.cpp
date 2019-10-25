d142977c75e1e0cbbdf9fc5e1ccb1a7a22c3f951
Stephanie S.T <51277482+pumpkinsteph@users.noreply.github.com>
2019-10-25 17:52:55 +0200

    Final version (got feedback from professor)

--- DragForce.cpp
+++ DragForce.cpp
@@ -10,9 +10,9 @@ float calculateArea(float width_in, float depth_in) {
 }
 
 float Fdx(sf::Vector2f velocity_fdx_in, float drag_area_fdx_in, float dragCoefficient_fdx_in) {
-	return (-0.5f * airDensity * drag_area_fdx_in * dragCoefficient_fdx_in * velocity_fdx_in.x * velocity_fdx_in.x);
+	return (-0.5f * airDensity * drag_area_fdx_in * dragCoefficient_fdx_in * lenght(velocity_fdx_in) * velocity_fdx_in.x);
 }
 
 float Fdy(sf::Vector2f velocity_fdy_in, float drag_area_fdy_in, float dragCoefficient_fdy_in) {
-	return (-0.5f * airDensity * drag_area_fdy_in * dragCoefficient_fdy_in * velocity_fdy_in.y * velocity_fdy_in.y); 
+	return (-0.5f * airDensity * drag_area_fdy_in * dragCoefficient_fdy_in * lenght(velocity_fdy_in) * velocity_fdy_in.y); 
 }
